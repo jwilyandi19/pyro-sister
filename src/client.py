49e5049cb376e59e5e4274c9ed32bfb784ac5e80
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-09-22 00:13:42 +0700

    add upload file (create)

--- src/client.py
+++ src/client.py
@@ -1,9 +1,29 @@
 import Pyro4
 
-def test_with_ns():
+def start_server():
     uri = "PYRONAME:filesystemserver@localhost:7777"
-    gserver = Pyro4.Proxy(uri)
-    print(gserver.get_greet('Bolang'))
+    fileServer = Pyro4.Proxy(uri)
+    print(fileServer.get_greet('Bolang'))
+    return fileServer
+
+def upload_file(file_name, srvr):
+    with open(file_name) as f:
+        content = f.read()
+        return srvr.createFile(content, file_name)
+
 
 if __name__=='__main__':
-    test_with_ns()
+    server = start_server()
+
+    print("Menu list:")
+    print("1. Upload file: UPLOAD <file_name>")
+
+    while True:
+        query = input("\n>>")
+        query1 = query.split()
+
+        if query1[0] == 'UPLOAD':
+            res = upload_file(query1[1], server)
+            print(res)
+
+
--- src/filesystem.py
+++ src/filesystem.py
@@ -1,5 +1,5 @@
 import random
-
+import os
 
 class FileSystemServer(object):
     def __init__(self):
@@ -8,6 +8,16 @@ class FileSystemServer(object):
     def get_greet(self, name='NoName'):
         lucky_number = random.randint(1, 100000)
         return "Hello {}, this is your lucky number {}".format(name, lucky_number)
+
+    def createFile(self, content, file_name):
+        print("Uploading {}...".format(file_name))
+        path = "file_system/{}".format(file_name)
+        if os.path.exists(path):
+            return "File {} already exist".format(file_name)
+        else:
+            with open(path, "w+") as f:
+                f.write(content)
+            return "File {} has been uploaded".format(file_name)
 
 
 if __name__ == '__main__':
