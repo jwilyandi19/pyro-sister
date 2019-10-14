997e7012b6008cd604bd91fac27bf2021ebd5cdf
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-10-15 01:54:04 +0700

    centralized-heartbeat

---
+++ src/centralized_heartbeat/client.py
@@ -0,0 +1,97 @@
+import Pyro4, Pyro4.errors
+import threading as tred
+
+class HBThread(tred.Thread):
+    def __init__(self, srvr):
+        tred.Thread.__init__(self)
+        self.srvr = srvr
+
+    def run(self):
+        while True:
+            try:
+                self.srvr.send_heartbeat()
+            except Pyro4.errors.ConnectionClosedError:
+                print("Thread: No Heartbeat. Exiting...")
+                break
+
+
+
+def start_server():
+    uri = "PYRONAME:filesystemserver@localhost:7777"
+    fileServer = Pyro4.Proxy(uri)
+    return fileServer
+
+#untuk UPLOAD
+def upload_file(file_name, srvr):
+    with open(file_name) as f:
+        content = f.read()
+        return srvr.createFile(content, file_name)
+
+#untuk READ
+def read_file(file_name, srvr):
+    return srvr.readFile(file_name)
+
+#untuk UPDATE
+def update_file(file_name, srvr):
+    with open(file_name) as f:
+        content = f.read()
+        return srvr.updateFile(content, file_name)
+
+#untuk DELETE
+def delete_file(file_name, srvr):
+    return srvr.deleteFile(file_name)
+
+#untuk LIST
+def list_file(srvr):
+    print(srvr)
+    return srvr.listFile()
+
+
+if __name__=='__main__':
+    server = start_server()
+
+    print("Simple file system. Only for file upload purpose. ")
+    print("Menu list:")
+    print("1. Upload file: UPLOAD <file_name>")
+    print("2. Read File: READ <file_name>")
+    print("3. Update File: UPDATE <file_name>")
+    print("4. Delete File: DELETE <file_name>")
+    print("5. List File: LIST")
+    print("6. Exit program: EXIT")
+    running = True
+    thread = HBThread(server)
+    thread.start()
+    while running:
+        try:
+            query = input("\n>> ")
+            query1 = query.split()
+            if query1[0] == 'UPLOAD':
+                res = upload_file(query1[1], server)
+                print(res)
+            elif query1[0] == 'READ':
+                res = read_file(query1[1],server)
+                print(res)
+            elif query1[0] == 'UPDATE':
+                res = update_file(query1[1], server)
+                print(res)
+            elif query1[0] == 'DELETE':
+                res = delete_file(query1[1], server)
+                print(res)
+            elif query1[0] == 'LIST':
+                files = list_file(server)
+                for filename in files:
+                    print(filename)
+            elif query1[0] == 'EXIT':
+                print("Exiting...")
+                running = False
+            else:
+                print("Query does not exist. Please try listed query")
+        except (Pyro4.errors.TimeoutError, Pyro4.errors.ConnectionClosedError):
+            print("Server Timeout. Exiting...")
+            running = False
+        except KeyboardInterrupt:
+            print("Exiting...")
+            running = False
+
+
+
---
+++ src/centralized_heartbeat/filesystem.py
@@ -0,0 +1,70 @@
+import random
+import os
+
+class FileSystemServer(object):
+    def __init__(self):
+        pass
+
+    def send_heartbeat(self):
+        return "I'm alive..."
+
+    def get_greet(self, name='NoName'):
+        lucky_number = random.randint(1, 100000)
+        return "Hello {}, this is your lucky number {}".format(name, lucky_number)
+
+    def createFile(self, content, file_name):
+        print("Uploading {}...".format(file_name))
+        path = "file_system/{}".format(file_name)
+        if os.path.exists(path):
+            return "File {} already exist".format(file_name)
+        else:
+            with open(path, "w+") as f:
+                f.write(content)
+            return "File {} has been uploaded".format(file_name)
+
+    def readFile(self, file_name):
+        print("Reading {}...".format(file_name))
+        path = "file_system/{}".format(file_name)
+
+        if not os.path.exists(path):
+            return "File {} is not exist".format(file_name)
+        else:
+            with open(path) as f:
+                content = f.read()
+                return content
+
+    def updateFile(self, content, file_name):
+        print("Updating {}...".format(file_name))
+        path = "file_system/{}".format(file_name)
+
+        if not os.path.exists(path):
+            return "File {} is not exist".format(file_name)
+        else:
+            with open(path, "w+") as f:
+                f.write(content)
+            return "File {} has been updated".format(file_name)
+
+    def deleteFile(self, file_name):
+        print("Deleting {}...".format(file_name))
+        path = "file_system/{}".format(file_name)
+
+        if not os.path.exists(path):
+            return "File {} is not exist".format(file_name)
+        else:
+            os.remove(path)
+            return "File {} has been deleted".format(file_name)
+
+    def listFile(self):
+        print("Listing...")
+        path = "file_system/"
+
+        files = []
+        for root,dirs,files in os.walk(path):
+            return files
+
+
+
+
+if __name__ == '__main__':
+    k = FileSystemServer()
+    print(k.get_greet('royyana'))
